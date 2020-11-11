32cf751a32f092960909e7f7b5fada19aa347529
Jonny Squire <jonathan.squire@raytheon.co.uk>
2020-11-11 10:16:28 +0000

    Skeleton table for manager

--- src/components/ManagerInterface/ManagerInterface.jsx
+++ src/components/ManagerInterface/ManagerInterface.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import { format, sub } from "date-fns";
+import React, { useMemo } from "react";
+import { Table } from "reactstrap";
 
 const ManagerInterface = () => {
+  const today = new Date();
+  const dateArray = useMemo(() => [sub(today, { days: 1 }), today], []);
+  const queryResults = [];
+
   return (
     <>
-      <p>Manager Content Works!</p>
+      {queryResults.length ? (
+        <Table hover bordered>
+          <thead>
+            <th>Name</th>
+            {dateArray.map(date => (
+              <th>{format(date, "dd/MM/yyyy")}</th>
+            ))}
+          </thead>
+          <tbody>
+            {queryResults.map(result => (
+              <tr>
+                <td>{result.name}</td>
+                {result.map(item => (
+                  <td>{item.mood}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      ) : (
+        <p>Sorry, no results match your query</p>
+      )}
     </>
   );
 };
