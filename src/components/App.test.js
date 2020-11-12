6af275bfb8516658f6bf399b989b89d3e7735ce7
Jonny Squire <jonathan.squire@raytheon.co.uk>
2020-11-12 10:32:35 +0000

    Add mock date for snapshots

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,16 @@
 import React from "react";
-
 import { render } from "@testing-library/react";
+import MockDate from "mockdate";
+
 import App from "./App";
 
 describe("App component", () => {
+  beforeAll(() => {
+    MockDate.set("2020-01-01");
+  });
+  afterAll(() => {
+    MockDate.reset();
+  });
   it("renders to match the snapshot", () => {
     const subject = render(<App />);
     expect(subject.baseElement).toMatchSnapshot();
--- src/components/ColleagueInterface/ColleagueInterface.test.js
+++ src/components/ColleagueInterface/ColleagueInterface.test.js
@@ -1,10 +1,17 @@
 import React from "react";
-
 import { render, screen } from "@testing-library/react";
+import MockDate from "mockdate";
 
 import ColleagueInterface from "./ColleagueInterface";
 
 describe("Colleague Interface component", () => {
+  beforeAll(() => {
+    MockDate.set("2020-01-01");
+  });
+  afterAll(() => {
+    MockDate.reset();
+  });
+
   it("matches the snapshot", () => {
     const subject = render(<ColleagueInterface />);
     expect(subject.baseElement).toMatchSnapshot();
