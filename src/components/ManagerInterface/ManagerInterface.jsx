014b42ce5115873f23f9b13fd54b6391c6724f64
Eshan Mistry <eshanmistry73@hotmail.com>
2020-11-26 22:19:29 +0000

    Added Dark Mode for component text

--- src/components/ManagerInterface/ManagerInterface.jsx
+++ src/components/ManagerInterface/ManagerInterface.jsx
@@ -1,17 +1,19 @@
 import { format, parse } from "date-fns";
 import { Fragment, useEffect, useState } from "react";
 import { Card, CardBody, CardText, CardTitle } from "reactstrap";
+import PropTypes from "prop-types";
 import { RetrieveRecordsForLastSevenDays } from "../utils/api";
 
 import styles from "./ManagerInterface.module.css";
+import lightstyles from "../LightApp.module.css";
 
-const ManagerInterface = () => {
+const ManagerInterface = ({ darkMode }) => {
   const today = new Date();
   const [moodResults, setMoodResults] = useState([]);
   const [moodError, setMoodError] = useState([]);
 
   useEffect(() => {
-    RetrieveRecordsForLastSevenDays(today).then(response => {
+    RetrieveRecordsForLastSevenDays(today).then((response) => {
       if (response.status === 500) {
         setMoodError(response.error);
       } else {
@@ -22,12 +24,23 @@ const ManagerInterface = () => {
 
   return (
     <>
-      <h2>How are you feeling today?</h2>
+      <h2
+        className={
+          darkMode ? styles.TextBackground : lightstyles.TextBackground
+        }
+      >
+        Responses:
+      </h2>
       {moodResults.length ? (
         <div className={styles.card_container}>
-          {moodResults.map(moodResult => {
+          {moodResults.map((moodResult) => {
             return (
-              <Card className={styles.card} key={`card-${moodResult?.date}`}>
+              <Card
+                className={
+                  darkMode ? styles.card : lightstyles.card
+                }
+                key={`card-${moodResult?.date}`}
+              >
                 <CardBody>
                   <CardTitle className={styles.card_title}>
                     {format(
@@ -36,7 +49,7 @@ const ManagerInterface = () => {
                     )}
                   </CardTitle>
                   <CardText>
-                    {moodResult?.moods.map(mood => {
+                    {moodResult?.moods.map((mood) => {
                       return (
                         <Fragment
                           key={`${mood.userName}-mood-${moodResult.date}`}
@@ -61,4 +74,9 @@ const ManagerInterface = () => {
     </>
   );
 };
+
+ManagerInterface.propTypes = {
+  darkMode: PropTypes.string.isRequired,
+};
+
 export default ManagerInterface;
