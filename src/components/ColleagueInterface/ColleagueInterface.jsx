efaa04bf00fb704a6af64de00599ef89be3687a1
simond2000 <73893403+simond2000@users.noreply.github.com>
2020-11-27 13:52:35 +0000

    update

--- src/components/ColleagueInterface/ColleagueInterface.jsx
+++ src/components/ColleagueInterface/ColleagueInterface.jsx
@@ -109,7 +109,7 @@ const ColleagueInterface = ({ darkMode }) => {
 };
 
 ColleagueInterface.propTypes = {
-  darkMode: PropTypes.string,
+  darkMode: PropTypes.string.isRequired,
 };
 
 export default ColleagueInterface;
